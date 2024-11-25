93dd72cc80f3d21aaae0daf9b4b33ecab500e63d
Arjun Kollipara <arjun.kollipara22@gmail.com>
2024-11-25 20:11:38 +0530

    commit 2 - updated prime.java

--- prime.java
+++ prime.java
@@ -11,9 +11,9 @@ public class Prime {
         }
 
         if (isPrime) {
-            System.out.println(num + " is a prime number.");
+            System.out.println(num + " is definetly a prime number.");
         } else {
-            System.out.println(num + " is not a prime number.");
+            System.out.println(num + " is a prime number.");
         }
     }
 }
