07a0a68a5f00338229bca9c1d48c727994d4eea6
ARJUN KOLLIPARA <123087955+Arjunkollipara@users.noreply.github.com>
2024-12-09 21:56:36 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -1,5 +1,7 @@
-public class Prime {
-    public static void main(String[] args) {
-       system.out.println("hello world");
+class Test
+{
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program.");
     }
-}
+};
